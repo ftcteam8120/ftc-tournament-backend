d4a820e52f98b3e31740df69f145168e25c04141
Kirk Brauer <kirkbrauer@outlook.com>
2017-11-09 23:29:35 -0500

    Added mocha and chai for testing
    Added requireFields middleware tests

---
+++ test/requiredFields.ts
@@ -0,0 +1,93 @@
+import 'mocha';
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+
+import { checkFields, requireFields } from '../src/utils/requireFields';
+
+describe('The checkFields function', function () {
+  describe('simple objects', function() {
+    it('should work normally', function () {
+      expect(checkFields(['name', 'address'], {
+        name: 'Mario',
+        address: 'Peach Street',
+        another: 'Hello'
+      })).to.eql([]);
+    });
+    it('should find missing fields', function() {
+      expect(checkFields(['name', 'address', 'test'], {
+        name: 'Mario',
+        address: 'Peach Street',
+        another: 'Hello'
+      })).to.eql(['test']);
+    });
+  });
+
+  describe('nested objects', function () {
+    it('should work normally', function () {
+      expect(checkFields(['name.first', 'name.last', 'address'], {
+        name: {
+          first: 'John',
+          last: 'Doe'
+        },
+        address: 'Test Road'
+      })).to.eql([]);
+    });
+    it('should find missing fields', function () {
+      expect(checkFields(['name.first', 'name.last', 'address'], {
+        name: {
+          last: 'Doe'
+        },
+        address: 'Test Road'
+      })).to.eql(['name.first']);
+    });
+  });
+
+  describe('multiple nested objects', function () {
+    it('should work normally', function () {
+      expect(checkFields(['email.address', 'email.site.name', 'email.site.type'], {
+        email: {
+          address: 'test',
+          site: {
+            name: 'gmail',
+            type: 'com'
+          }
+        }
+      })).to.eql([]);
+    });
+    it('should find missing fields', function () {
+      expect(checkFields(['email.address', 'email.site.name', 'email.site.type', 'email.site.secure'], {
+        email: {
+          address: 'test',
+          site: {
+            name: 'gmail',
+            type: 'com'
+          }
+        }
+      })).to.eql(['email.site.secure']);
+    });
+  });
+});
+
+describe('The requireFields middleware', function () {
+  describe('request handler creation', function() {
+    var mw;
+    beforeEach(function () {
+      mw = requireFields(['test']);
+    });
+    it('should return a function()', function() {
+      expect(mw).to.be.a('function');
+    });
+    it('should accept three arguments', function() {
+      expect(mw.length).to.equal(3);
+    });
+  });
+
+  describe('request handler calling', function() {
+    it('should call next() once', function() {
+      var mw = requireFields(['test']);
+      var nextSpy = sinon.spy();
+      mw(({ body: { test: 'test' } } as any), ({} as any), nextSpy);
+      expect(nextSpy.calledOnce).to.be.true;
+    });
+  });
+});
